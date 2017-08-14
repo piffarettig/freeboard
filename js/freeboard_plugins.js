e070ad201d67d71f24b9b64edf031970a48daea8
Gabriel Piffaretti <gabrielpiffaretti@Gabriels-MacBook-Pro.local>
2017-08-14 15:54:14 -0300

    Added basic support for Azure Iot Hub (mocking api calls). Datasource gets added properly.

--- js/freeboard_plugins.js
+++ js/freeboard_plugins.js
@@ -3527,94 +3527,45 @@ $.extend(freeboard, jQuery.eventEmitter);
 			newInstanceCallback(new clockDatasource(settings, updateCallback));
 		}
 	});
+
 freeboard.loadDatasourcePlugin({
-		// **type_name** (required) : A unique name for this plugin. This name should be as unique as possible to avoid collisions with other plugins, and should follow naming conventions for javascript variable and function declarations.
-		"type_name"   : "meshblu",
-		// **display_name** : The pretty name that will be used for display purposes for this plugin. If the name is not defined, type_name will be used instead.
-		"display_name": "Octoblu",
-        // **description** : A description of the plugin. This description will be displayed when the plugin is selected or within search results (in the future). The description may contain HTML if needed.
-        "description" : "app.octoblu.com",
-		// **external_scripts** : Any external scripts that should be loaded before the plugin instance is created.
+
+		"type_name": "azure_iot_hub",
+		"display_name": "Azure IoT Hub",
+        "description" : "Azure IoT Hub: Azure gateway for connecting millions of devices and ingestin tons of data, establishing bidirectional communication, working with most familiar protocols and with strong features of security and device management.",
 		"external_scripts" : [
-			"http://meshblu.octoblu.com/js/meshblu.js"
+			"http://meshblu.octoblu.com/js/meshblu.js" //needs to be fixed to use the js of event hubs
 		],
-		// **settings** : An array of settings that will be displayed for this plugin when the user adds it.
 		"settings"    : [
 			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "uuid",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "UUID",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
+				"name"         : "connectionString",
+				"display_name" : "Connection String",
 				"type"         : "text",
-				// **default_value** : A default value for this setting.
-				"default_value": "device uuid",
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "your device UUID",
-                // **required** : Set to true if this setting is required for the datasource to be created.
+				"default_value": "HostName={your-host-name};SharedAccessKeyName={your-access-key-name};SharedAccessKey={your-acces-key};",
+				"description"  : "Your connection String to your IoT Hub",
                 "required" : true
-			},
+			}, 
 			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "token",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "Token",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
+				"name"         : "refresh_time",
+				"display_name" : "Refresh Time",
 				"type"         : "text",
-				// **default_value** : A default value for this setting.
-				"default_value": "device token",
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "your device TOKEN",
-                // **required** : Set to true if this setting is required for the datasource to be created.
-                "required" : true
-			},
-			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "server",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "Server",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-				"type"         : "text",
-				// **default_value** : A default value for this setting.
-				"default_value": "meshblu.octoblu.com",
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "your server",
-                // **required** : Set to true if this setting is required for the datasource to be created.
-                "required" : true
-			},
-			{
-				// **name** (required) : The name of the setting. This value will be used in your code to retrieve the value specified by the user. This should follow naming conventions for javascript variable and function declarations.
-				"name"         : "port",
-				// **display_name** : The pretty name that will be shown to the user when they adjust this setting.
-				"display_name" : "Port",
-				// **type** (required) : The type of input expected for this setting. "text" will display a single text box input. Examples of other types will follow in this documentation.
-				"type"         : "number",
-				// **default_value** : A default value for this setting.
-				"default_value": 80,
-				// **description** : Text that will be displayed below the setting to give the user any extra information.
-				"description"  : "server port",
-                // **required** : Set to true if this setting is required for the datasource to be created.
-                "required" : true
+				"description"  : "In milliseconds",
+				"default_value": 5000
 			}
-			
 		],
-		// **newInstance(settings, newInstanceCallback, updateCallback)** (required) : A function that will be called when a new instance of this plugin is requested.
-		// * **settings** : A javascript object with the initial settings set by the user. The names of the properties in the object will correspond to the setting names defined above.
-		// * **newInstanceCallback** : A callback function that you'll call when the new instance of the plugin is ready. This function expects a single argument, which is the new instance of your plugin object.
-		// * **updateCallback** : A callback function that you'll call if and when your datasource has an update for freeboard to recalculate. This function expects a single parameter which is a javascript object with the new, updated data. You should hold on to this reference and call it when needed.
+		
 		newInstance   : function(settings, newInstanceCallback, updateCallback)
 		{
-			// myDatasourcePlugin is defined below.
-			newInstanceCallback(new meshbluSource(settings, updateCallback));
+			newInstanceCallback(new azureIoTHubSource(settings, updateCallback));
 		}
 	});
 
 
-	// ### Datasource Implementation
+	// ### Azure IoT Hub DataSource Implementation
 	//
 	// -------------------
 	// Here we implement the actual datasource plugin. We pass in the settings and updateCallback.
-	var meshbluSource = function(settings, updateCallback)
+	var azureIoTHubSource = function(settings, updateCallback)
 	{
 		// Always a good idea...
 		var self = this;
@@ -3622,35 +3573,38 @@ freeboard.loadDatasourcePlugin({
 		// Good idea to create a variable to hold on to our settings, because they might change in the future. See below.
 		var currentSettings = settings;
 
-		
-
-		/* This is some function where I'll get my data from somewhere */
-
- 	
+		/* Function where we connect to Azure IoT via its endpoint (EventHub), and gather data) */
 		function getData()
 		{
+			// populo el newData
 
-
-		 var conn = skynet.createConnection({
-    		"uuid": currentSettings.uuid,
-    		"token": currentSettings.token,
-    		"server": currentSettings.server, 
-    		"port": currentSettings.port
-  				});	
-			 
-			 conn.on('ready', function(data){	
-
-			 	conn.on('message', function(message){
-
-    				var newData = message;
-    				updateCallback(newData);
-
- 						 });
-
-			 });
+			function getRandomBetweenInterval(min,max) {
+				return Math.floor(Math.random()*(max-min+1)+min);
 			}
 
-	
+			var temperature = getRandomBetweenInterval(-8,35);
+    		var humidity = getRandomBetweenInterval(0,100);    
+
+			var newData = { temperature, humidity};
+			// Obtuve los datos y despues llamo al update callback
+			updateCallback(newData);
+		}
+
+		var refreshTimer;
+
+		function createRefreshTimer(interval)
+		{
+			if(refreshTimer)
+			{
+				clearInterval(refreshTimer);
+			}
+
+			refreshTimer = setInterval(function()
+			{
+				//Here we call our getData function to update freeboard with new data.
+				getData();
+			}, interval);
+		}
 
 		// **onSettingsChanged(newSettings)** (required) : A public function we must implement that will be called when a user makes a change to the settings.
 		self.onSettingsChanged = function(newSettings)
@@ -3662,19 +3616,19 @@ freeboard.loadDatasourcePlugin({
 		// **updateNow()** (required) : A public function we must implement that will be called when the user wants to manually refresh the datasource
 		self.updateNow = function()
 		{
-			// Most likely I'll just call getData() here.
+			// We just call getData() here.
 			getData();
 		}
 
 		// **onDispose()** (required) : A public function we must implement that will be called when this instance of this plugin is no longer needed. Do anything you need to cleanup after yourself here.
 		self.onDispose = function()
 		{
-		
-			//conn.close();
+			clearInterval(refreshTimer);
+			refreshTimer = undefined;
 		}
 
 		// Here we call createRefreshTimer with our current settings, to kick things off, initially. Notice how we make use of one of the user defined settings that we setup earlier.
-	//	createRefreshTimer(currentSettings.refresh_time);
+		createRefreshTimer(currentSettings.refresh_time);
 	}
 
 
